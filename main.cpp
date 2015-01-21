deed46fae7f1c98d841f87e143b1a96a13abfa1d
William Lee <wlee030@hammer.cs.ucr.edu>
2015-01-20 20:52:27 -0800

    made getuserinut and userhostname

--- main.cpp
+++ main.cpp
@@ -1,13 +1,20 @@
 #include <iostream>
 #include <string>
+#include <unistd.h>
+#include <stdio.h>
 
 int main()
 {
 	bool exit = false;
 	std::string input;
+	char hostname[128];
+	char username[128];
+
+	gethostname(hostname,128);
+	getlogin_r(username,128);
 	while(!exit)
 	{
-		std::cout << "$";
+		std::cout << username << "@" << hostname << "$";
 		getline(std::cin,input);
 	}
 }
