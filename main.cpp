6a5e2ef5fa44041db94c517a754611022e39c680
William Lee <wlee030@hammer.cs.ucr.edu>
2015-01-21 14:21:04 -0800

    added tokening

--- main.cpp
+++ main.cpp
@@ -1,26 +1,29 @@
 #include <iostream>
 #include <string>
+#include <string.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <vector>
 
 int main()
 {
 	bool exit = false;
-	std::string input;
-	char hostname[128];
-	char username[128];
-
+	std::string input;		//string value to get input
+	char hostname[128];		//get the hostname from terminal
+	char username[128];		//get username from terminal
 	gethostname(hostname,128);
 	getlogin_r(username,128);
-	while(!exit)
+	while(!exit)			//run until exit is found
 	{
-		std::cout << username << "@" << hostname << "$";
-		getline(std::cin,input);
-		if(input == "exit") exit = true;
-		else
+		std::cout << username << "@" << hostname << "$"; //output beg
+		getline(std::cin,input);			//get input
+		char *tok = strtok(const_cast<char*>(input.c_str())," &|;"); //will break the words if not &,|,;
+		std::vector<char*> commands;				     //vector to store the commands
+		while(tok != NULL)					     //loop till end
 		{
-			
-		}
+			commands.push_back(tok);
+			tok = strtok(NULL," &|;");
+		}		
 	}
 	return 0;
 }
