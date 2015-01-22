501489edbb16c718a778b299bc1a8dfcfe4435e9
William Lee <wlee030@hammer.cs.ucr.edu>
2015-01-21 23:43:17 -0800

    needs to add loop while queue is >0

--- src/main.cpp
+++ src/main.cpp
@@ -5,32 +5,33 @@
 #include <stdio.h>
 #include <vector>
 #include <queue>
-
+#include <signal.h>
 int main()
 {
-	bool exit = false;
 	std::string input;		//string value to get input
 	char hostname[128];		//get the hostname from terminal
 	char username[128];		//get username from terminal
 	gethostname(hostname,128);
 	getlogin_r(username,128);
-	while(!exit)			//run until exit is found
+	while(1)			//run until exit(0)
 	{
 		std::cout << username << "@" << hostname << "$"; //output beg
 		getline(std::cin,input);			//get input
 		std::queue<char> connector;			//queue of connector
 		
-		for(int i = 0;i < input.size();i++)
-		{
-			if(input[i].compare(";")== 0) connector.push(';');
-		}		
-
 		char *tok = strtok(const_cast<char*>(input.c_str()),";&|"); //will break the words if not a space
 		std::queue<char*> commands;				     //queue to store the commands
 		while(tok != NULL)					     //loop till end
 		{
 			commands.push(tok);
 			tok = strtok(NULL,";&|");
+		}
+
+		if(commands.top() == "exit")
+			exit(0);
+		else
+		{
+			shellstart();	
 		}
 	/*	while(commands.size() > 0)				    //loop through the words
 		{
