9bf12ccc18682a80fb831a8f51f24fe458fbd693
William Lee <wlee030@kilo-14.cs.ucr.edu>
2015-02-02 21:02:36 -0800

    makefile and ls.cpp update

--- Makefile
+++ Makefile
@@ -3,16 +3,22 @@ CXX = g++
 CFLAGS = -ansi -pedantic -Wall -Werror
 VPATH = src
 FILEPATH = ./src/main.cpp
+FILEPATH1 = ./src/ls.cpp
 OUTPATH = ./bin/rshell
+OUTPATH1 = ./bin/ls
 objects = main.o
 
 #Targets
 all: 
 	mkdir -p ./bin
 	$(CXX) $(CFLAGS) $(FILEPATH) -o $(OUTPATH)
+	$(CXX) $(CFLAGS) $(FILEPATH1) -o $(OUTPATH1)
 
 rshell: 
 	mkdir -p ./bin
-	$(CXX) $(CFLAGS) $(FILEPATH) -o $(OUTPATH)  
+	$(CXX) $(CFLAGS) $(FILEPATH) -o $(OUTPATH) 
+ls:
+	mkdir -p ./bin
+	$(CXX) $(CFLAGS) $(FILEPATH1) -o $(OUTPATH1) 
 clean:
 	rm -rf ./bin
--- src/ls.cpp
+++ src/ls.cpp
@@ -4,10 +4,45 @@
 #include <dirent.h>
 #include <errno.h>
 #include <stdio.h>
+#include <vector>
+#include <stdlib.h>
+
+//turn flags on
+void whichFlags(char* command,std::vector<bool> &flaggs)
+{
+	//find size of command
+	size_t counter = 0;
+	for(size_t s = 0;command[s] != '\0';s++)
+		counter++;
+	
+	bool isValid = true;
+	size_t k = 0;
+	//goes through the command and sees if valid. If not a valid command,nothing gets turned on
+	while(isValid && k < counter)
+	{
+		if(command[k] != 'l' || command[k] != 'R' || command[k] != 'a' || command[k] != '-')
+			isValid = false;
+	}
+	
+	//if the command is valid
+	if(isValid)
+	{
+		for(size_t j = 1;j < counter;j++)
+		{
+			if(command[j] == 'l' && flaggs[0] == false)
+				flaggs[0] = true;
+			else if(command[j] == 'a' && flaggs[1] == false)
+				flaggs[1] = true;
+			else if(command[j] == 'R' && flaggs[2] == false)
+				flaggs[2] = true;
+		}
+	}
+}
 
 //get an array of char pointers(arrays)
 int main(int argc,char** argv)
 {
+	size_t newArgc = argc;
 	if(argc < 1)	//there are no commands
 	{
 		perror("Error: Something went wrong");
@@ -15,10 +50,25 @@ int main(int argc,char** argv)
 	}
 	else
 	{	
+		std::vector<bool>theFlags(3,false);	//initialize flag. 1=-l,2=-a,3=-R
+	
 		//check for path or flag
-		for(int i = 0;i < argc;i++)
+		for(size_t i = 0;i < newArgc;i++)
 		{
-			
+			char* comm = argv[i];
+			if(comm[i] == '-')	//if find -, then it is a flag
+			{
+				whichFlags(comm,theFlags);
+				std::cout << "here";
+				for(size_t a = 0;a < 3;a++)
+					std::cout << theFlags[a] << " ";
+			}
+		/*	else if()		//else if its a path
+			{
+			}
+			else			//if its none of them. aka error
+			{
+			}*/
 		}
 	}	
 	/*
