b12aa6fe3bec9af6ef62d360aa3c5e15a26e68fd
William Lee <wlee030@hammer.cs.ucr.edu>
2015-02-06 18:38:27 -0800

    flags are turned on properly

--- src/ls.cpp
+++ src/ls.cpp
@@ -8,25 +8,30 @@
 #include <stdlib.h>
 
 //turn flags on
-void whichFlags(char* command,std::vector<bool> &flaggs)
+bool whichFlags(char* command,std::vector<bool> &flaggs)
 {
 	//find size of command
 	size_t counter = 0;
 	for(size_t s = 0;command[s] != '\0';s++)
 		counter++;
-	
 	bool isValid = true;
-	size_t k = 0;
+	size_t k = 1;
 	//goes through the command and sees if valid. If not a valid command,nothing gets turned on
+	//first char needs to be -
+	if(command[0] != '-')
+		isValid = false;
 	while(isValid && k < counter)
 	{
-		if(command[k] != 'l' || command[k] != 'R' || command[k] != 'a' || command[k] != '-')
+		if(command[k] != 'l' && command[k] != 'R' && command[k] != 'a')
+		{
 			isValid = false;
+		}
+		k++;
 	}
-	
 	//if the command is valid
 	if(isValid)
 	{
+		//starts from second command. first should be '-'
 		for(size_t j = 1;j < counter;j++)
 		{
 			if(command[j] == 'l' && flaggs[0] == false)
@@ -37,11 +42,13 @@ void whichFlags(char* command,std::vector<bool> &flaggs)
 				flaggs[2] = true;
 		}
 	}
+	return isValid;
 }
 
 //get an array of char pointers(arrays)
 int main(int argc,char** argv)
 {
+	bool canFlag;
 	size_t newArgc = argc;
 	std::string comm;
 //	for(size_t h = 1;h < newArgc;h++)
@@ -51,31 +58,42 @@ int main(int argc,char** argv)
 		perror("Error: Something went wrong");
 		exit(1);
 	}
-	else	//something was inputted
-	{	
+	else if(newArgc > 1)	//something was inputted
+	{		
 		std::vector<bool>theFlags(3,false);	//initialize flag. 1=-l,2=-a,3=-R
-	
+		std::vector<std::string> paths;		//these are all the paths
 		//check for path or flag
-		//first word needs to be ls or cannot work
-		comm = argv[0];
+		//first word needs to be ls or cannot work and cannot be just bin/ls
+		comm = argv[1];
 		if(comm == "ls")
 		{
-			for(size_t i = 1;i < newArgc;i++)
+			for(size_t i = 2;i < newArgc;i++)
 			{
 				comm = argv[i];
-				if(comm[i] == '-')	//if find -, then it is a flag
+				if(comm[0] == '-')	//if find -, then it is a flag
 				{
-					whichFlags(argv[i],theFlags);
+					canFlag = whichFlags(argv[i],theFlags);
+					if(!canFlag)
+					{
+						perror("bad input");
+						exit(1);
+					}
 				}
-				else if()	//if a path
+				else		//it is a path(right or wrong)
 				{
-				}
-				else		//if not any
-				{
+					paths.push_back(comm);
 				}
 			}
+			
 		}
-	}	
+		for(int z = 0;z < 3;z++)
+                	std::cout << theFlags[z] << std::endl;
+		
+		//start to open the paths...somehow
+	}
+	else	//nothing input. only bin/ls
+		exit(1);
+	
 	/*
 	char *dirName = ".";
 	DIR *dirp = opendir(dirName);
